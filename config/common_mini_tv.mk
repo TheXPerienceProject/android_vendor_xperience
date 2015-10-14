4f2e1eaea97569866382fe5bc387188e6aea5cf6
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2015-10-14 18:04:44 -0500

    remove alarm and sounds

--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -4,11 +4,6 @@ $(call inherit-product, vendor/XPe/config/common.mk)
 # Include CM audio files
 include vendor/XPe/config/cm_audio.mk
 
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
