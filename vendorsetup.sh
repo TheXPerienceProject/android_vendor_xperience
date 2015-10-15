a0ae50e54781e1a1a3dde22ae534738df6ef041f
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-10-15 15:14:29 -0500

    change vendor
    Change-Id: Icc685f9ed4b71b0c2fa8ce8495c46288729f544d
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,2 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-13.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo xpe_falcon-userdebug
+add_lunch_combo xpe_grouper-userdebug
