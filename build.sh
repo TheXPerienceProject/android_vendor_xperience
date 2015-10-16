48a6eccf2b174583b26797cf50fa49e4cb060c46
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2015-10-16 14:11:32 -0500

    build version

--- build.sh
+++ build.sh
@@ -71,7 +71,7 @@ if [ $ARCH = "64" ]; then
   # Get start time
   res1=$(date +%s.%N)
 
-  echo -e "${cya}Building ${bldcya}XPerience $VERSION for $DEVICE ${txtrst}";
+  echo -e "${cya}Building ${bldcya}XPerience 10.0.0_beta1 for $DEVICE ${txtrst}";
   echo -e "${bldgrn}Start time: $(date) ${txtrst}"
 
   # Decide what command to execute
