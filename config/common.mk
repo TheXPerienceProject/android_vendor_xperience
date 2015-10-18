80df954c1db6dae81640e9125f1b860643a60b31
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-10-18 18:46:13 -0500

    vendor: optimizations for devices with low-RAM [2/2]
    * Remove some apps based on:
      https://gerrit.omnirom.org/#/c/11993
      https://gerrit.omnirom.org/#/c/12046
    
    * Reference for low-ram devices: https://source.android.com/devices/tech/low-ram.html
    
    * Use dlmalloc instead of jemalloc, it performs better on low-ram devices
    
    * Reduce background apps limit to 16 on low-tier devices
    
    * Set max background services to 8 on low-tier devices
    
    * Disable bootanimation cached texture, saves memory used at boot
    
    * Disable atlas services on low-ram targets
    
    Flag: XPE_LOW_RAM_DEVICE := true
    
    Add this in your BoardConfig
    
    Taked from https://github.com/PAC-ROM/android_vendor_pac/commit/d1f86e980cea92725b90a313ec81c426a97c6504
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    Change-Id: I837b1aa6af4cbdfb45dfcdb0f63f64bface130a4

--- config/common.mk
+++ config/common.mk
@@ -65,6 +65,20 @@ PRODUCT_COPY_FILES += \
     vendor/XPe/CHANGELOG.mkdn:system/etc/CHANGELOG-XPE.txt \
     vendor/XPe/DONATORS.mkdn:system/etc/donators.txt
 
+# Lower RAM devices
+ifeq ($(XPE_LOW_RAM_DEVICE),true)
+MALLOC_IMPL := dlmalloc
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    config.disable_atlas=true \
+    dalvik.vm.jit.codecachesize=0 \
+    persist.sys.force_highendgfx=true \
+    ro.config.low_ram=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+endif
+
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
