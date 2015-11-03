e16a876c45b1f759a8f7aaad9ea36c257a52bf1c
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-11-02 22:09:55 -0600

    USE LATINIME AGAIN
    Change-Id: I2edf020b8583401275ddc1be43f515986076b344
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -4,15 +4,12 @@ $(call inherit-product, vendor/XPe/config/common.mk)
 # Include XPe audio files
 include vendor/XPe/config/xpe_audio.mk
 
-# Include XPe LatinIME dictionaries
-PRODUCT_COPY_FILES += \
-    vendor/XPe/prebuilt/XPEKey/app/ExternalKeyboardsInternational/ExternalKeyboardsInternational.apk:system/app/ExternalKeyboardsInternational/ExternalKeyboardsInternational.apk \
-    vendor/XPe/prebuilt/XPEKey/app/textinput-tng/textinput-tng.apk:system/app/textinput-tng/textinput-tng.apk \
-    vendor/XPe/prebuilt/XPEKey/app/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-    vendor/XPe/prebuilt/XPEKey/app/xperia-keyboard-dictionaries/xperia-keyboard-dictionaries.apk:system/app/xperia-keyboard-dictionaries/xperia-keyboard-dictionaries.apk \
-    vendor/XPe/prebuilt/XPEKey/etc/permissions/com.sony.device.xml:system/etc/permissions/com.sony.device.xml \
-    vendor/XPe/prebuilt/XPEKey/framework/com.sony.device.jar:system/framework/com.sony.device.jar \
-    vendor/XPe/prebuilt/XPEKey/lib/libswiftkeysdk-java.so:system/lib/libswiftkeysdk-java.so
+# Required CM packages
+PRODUCT_PACKAGES += \
+LatinIME
+
+# Include CM LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/XPe/overlay/dictionaries
 
 # Optional XPe packages
 PRODUCT_PACKAGES += \
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -11,16 +11,6 @@ LatinIME
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/XPe/overlay/dictionaries
 
-# Include XPe LatinIME dictionaries
-PRODUCT_COPY_FILES += \
-    vendor/XPe/prebuilt/XPEKey/app/ExternalKeyboardsInternational/ExternalKeyboardsInternational.apk:system/app/ExternalKeyboardsInternational/ExternalKeyboardsInternational.apk \
-    vendor/XPe/prebuilt/XPEKey/app/textinput-tng/textinput-tng.apk:system/app/textinput-tng/textinput-tng.apk \
-    vendor/XPe/prebuilt/XPEKey/app/textinput-tng/lib/arm/libswiftkeysdk-java.so:system/app/textinput-tng/lib/arm/libswiftkeysdk-java.so \
-    vendor/XPe/prebuilt/XPEKey/app/xperia-keyboard-dictionaries/xperia-keyboard-dictionaries.apk:system/app/xperia-keyboard-dictionaries/xperia-keyboard-dictionaries.apk \
-    vendor/XPe/prebuilt/XPEKey/etc/permissions/com.sony.device.xml:system/etc/permissions/com.sony.device.xml \
-    vendor/XPe/prebuilt/XPEKey/framework/com.sony.device.jar:system/framework/com.sony.device.jar \
-    vendor/XPe/prebuilt/XPEKey/lib/libswiftkeysdk-java.so:system/lib/libswiftkeysdk-java.so
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/XPe/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
