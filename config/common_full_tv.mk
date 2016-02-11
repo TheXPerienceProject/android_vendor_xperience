b5fb6238f8cf78575ed6131fd7ad224260c7a33d
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-02-11 12:31:14 -0600

    fix some issues
    Change-Id: I04169d1c23b7ffda7a06e3f384c96e9c09b91d54
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -3,5 +3,5 @@ $(call inherit-product, vendor/XPe/config/common_full.mk)
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+        vendor/XPe/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
 endif
--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -6,5 +6,5 @@ include vendor/XPe/config/xpe_audio.mk
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+        vendor/XPe/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
 endif
