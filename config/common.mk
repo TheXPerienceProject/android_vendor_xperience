b64c4a936262dc84759ba8a9db20143d51853725
Adnan Begovic <adnan@cyngn.com>
2016-03-15 14:17:47 -0700

    cm: Commonize cmsdk specific targets.
    Change-Id: I5e2f0b84ed34244b7549daf91053258193dd716f
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -163,12 +163,11 @@ PRODUCT_COPY_FILES += \
         vendor/XPe/prebuilt/common/bin/707-xpe.sh:system/addon.d/707-xpe.sh 
 
 
-# Live lockscreen
-PRODUCT_COPY_FILES += \
-    vendor/XPe/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml
-
 # Theme engine
 -include vendor/XPe/config/themes_common.mk
+
+# CMSDK
+include vendor/cm/config/cmsdk_common.mk
 
 # Required packages
 PRODUCT_PACKAGES += \
@@ -202,17 +201,6 @@ PRODUCT_PACKAGES += \
 # Exchange support
 PRODUCT_PACKAGES += \
     Exchange2
-
-# CM Platform Library
-PRODUCT_PACKAGES += \
-    org.cyanogenmod.platform-res \
-    org.cyanogenmod.platform \
-    org.cyanogenmod.platform.xml
-
-# CM Hardware Abstraction Framework
-PRODUCT_PACKAGES += \
-    org.cyanogenmod.hardware \
-    org.cyanogenmod.hardware.xml
 
 # Extra tools
 PRODUCT_PACKAGES += \
@@ -381,34 +369,9 @@ ifneq ($(PRODUCT_DEFAULT_DEV_CERTIFICATE),build/target/product/security/testkey)
 endif
 endif
 
-ifndef CM_PLATFORM_SDK_VERSION
-  # This is the canonical definition of the SDK version, which defines
-  # the set of APIs and functionality available in the platform.  It
-  # is a single integer that increases monotonically as updates to
-  # the SDK are released.  It should only be incremented when the APIs for
-  # the new release are frozen (so that developers don't write apps against
-  # intermediate builds).
-  CM_PLATFORM_SDK_VERSION := 5
-endif
-
-ifndef CM_PLATFORM_REV
-  # For internal SDK revisions that are hotfixed/patched
-  # Reset after each XPE_PLATFORM_SDK_VERSION release
-  # If you are doing a release and this is NOT 0, you are almost certainly doing it wrong
-  CM_PLATFORM_REV := 0
-endif
-
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.display.version=$(XPE_DISPLAY_VERSION) \
   ro.xpe.display.version=$(XPE_DISPLAY_VERSION)
-
-# CyanogenMod Platform SDK Version
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
-
-# CyanogenMod Platform Internal
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.build.version.plat.rev=$(CM_PLATFORM_REV)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 
--- config/themes_common.mk
+++ config/themes_common.mk
@@ -4,5 +4,3 @@ PRODUCT_PACKAGES += \
     ThemeChooser \
     ThemesProvider
 
-PRODUCT_COPY_FILES += \
-    vendor/XPe/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
