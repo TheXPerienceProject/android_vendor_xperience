c15dd6dc2ac59a089a5eb53658a88238c4ffdefa
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-12-03 05:36:13 -0600

    xpe: boot animation generation Keep the largest bootanimation extracted and resize it at build-time. author: gmillz
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,11 +1,6 @@
 # Inherit common XPe stuff
 $(call inherit-product, vendor/XPe/config/common_full.mk)
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/XPe/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
-endif
-
 # Required CM packages
 PRODUCT_PACKAGES += \
     LatinIME
--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -7,9 +7,4 @@ LatinIME
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/XPe/overlay/dictionaries
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/XPe/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
-endif
-
 $(call inherit-product, vendor/XPe/config/telephony.mk)
