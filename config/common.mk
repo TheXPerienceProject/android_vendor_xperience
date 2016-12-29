d4cb417948e3bb403252f6dd242566579b605696
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2016-12-28 21:27:29 -0600

    fix bootanimation

--- config/common.mk
+++ config/common.mk
@@ -1,13 +1,31 @@
 RODUCT_BRAND ?= XPerience & MXSeñorPato
 
-ifeq ($(XPE_WIDTH),640)
-PRODUCT_BOOTANIMATION := vendor/XPe/prebuilt/common/bootanimation/640.zip
-endif
-ifeq ($(XPE_WIDTH),720)
-PRODUCT_BOOTANIMATION := vendor/XPe/prebuilt/common/bootanimation/720.zip
-endif
-ifeq ($(XPE_WIDTH),1080)
-PRODUCT_BOOTANIMATION := vendor/XPe/prebuilt/common/bootanimation/1080.zip
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,,$(shell ls -1 vendor/XPe/prebuilt/common/bootanimation | sort -rn))
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
+    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+PRODUCT_BOOTANIMATION := vendor/XPe/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
 endif
 
 #well I add ringtones here for all devices
