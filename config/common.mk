f627209a544e24ffa3d44c797f4f1a3fa26658d8
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-13 15:41:01 +0800

    cm: config: Remove obsolete props
    All usages of ro.com.android.dataroaming default to false.
    The rest of the props are no longer used.
    
    Change-Id: I2320e82a1859f8c13f3430a43aa8714186158ee0
    (cherry picked from commit 76b66c1ab812e6e4749bec53e03bfc5084c38257)
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -37,14 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    keyguard.no_require_sim=true \
-    ro.com.google.clientidbase=android-google \
-    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    keyguard.no_require_sim=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
