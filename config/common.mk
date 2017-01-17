399cf08229670b7efbf392a15c662c4e7da33645
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2017-01-16 20:23:12 -0600

    Revert "xpe: Rework boot animation generation"
    This reverts commit 805437e5737cf47d9c7e75e7fc3c1a311280b7f7.

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,33 @@
 RODUCT_BRAND ?= XPerience & MXSeñorPato
 
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,,$(shell ls -1 vendor/XPe/prebuilt/common/bootanimation | sort -rn))
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
+    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+PRODUCT_BOOTANIMATION := vendor/XPe/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
+endif
+
 #well I add ringtones here for all devices
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Zen.ogg \
