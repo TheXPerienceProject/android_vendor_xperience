805437e5737cf47d9c7e75e7fc3c1a311280b7f7
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2017-01-16 18:32:42 -0600

    xpe: Rework boot animation generation
    Keep the largest bootanimation extracted
    and resize it at build-time.
    
    taken from lineageOS

--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -1,7 +1,2 @@
 # Inherit common XPe stuff
 $(call inherit-product, vendor/XPe/config/common_mini.mk)
-
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/XPe/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
-endif
