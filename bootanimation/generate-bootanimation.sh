2aec5e6a043998289a37c6b06b0dbb65d48f403a
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2017-01-16 18:34:14 -0600

    bootanim: Use a for loop to make part# folders

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -19,7 +19,10 @@ fi
 
 RESOLUTION=""$IMAGESIZE"x"$IMAGESIZE""
 
-mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..2}
+for part_cnt in 0 1 2
+ do
+     mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
+ done
 tar xfp "vendor/XPe/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
 
 # Create desc.txt
