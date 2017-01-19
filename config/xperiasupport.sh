29786ed012e560b8a8ab8182dd57a2db42a579d3
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-01-19 17:49:35 -0600

    xperiasupport:Generate dirs
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/xperiasupport.sh
+++ config/xperiasupport.sh
@@ -3,5 +3,8 @@
 ./vendor/XPe/tools/colors
 
 # Print something to build output
+echo ${bldppl}"Making dir..."${txtrst}
+mkdir -v -p $OUT/target/product/"$DEVICE"/system/etc/permissions
+mkdir -v -p $OUT/target/product/"$DEVICE"/system/etc/framework
 echo ${bldppl}"Moving XPeria support files..."${txtrst}
 cp -R vendor/prebuilt/XPeriaSupport $OUT/system/
