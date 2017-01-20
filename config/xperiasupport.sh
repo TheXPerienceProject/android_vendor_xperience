3f2caaf1ab1596c01cab17ab81e85f27ccc28c02
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-01-19 18:03:57 -0600

    ove mkdir to build script
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/xperiasupport.sh
+++ config/xperiasupport.sh
@@ -4,5 +4,5 @@
 
 # Print something to build output
 echo ${bldppl}"Moving XPeria support files..."${txtrst}
-cp -A -V -R vendor/XPe/prebuilt/XPeriaSupport/etc/permissions/. $OUT/target/product/"$DEVICE"/system/etc/permissions/
-cp -A -V -R vendor/XPe/prebuilt/XPeriaSupport/framework/. $OUT/target/product/"$DEVICE"/system/framework/
+cp -a -v -R vendor/XPe/prebuilt/XPeriaSupport/etc/permissions/. $OUT/target/product/"$DEVICE"/system/etc/permissions/
+cp -a -v -R vendor/XPe/prebuilt/XPeriaSupport/framework/. $OUT/target/product/"$DEVICE"/system/framework/
