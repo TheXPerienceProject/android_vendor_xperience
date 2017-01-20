aeb9ad5ae60d06723f7b29ba990b5b2cae3fdc8e
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-01-19 18:06:34 -0600

    Disable verbosity
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/xperiasupport.sh
+++ config/xperiasupport.sh
@@ -4,5 +4,5 @@
 
 # Print something to build output
 echo ${bldppl}"Moving XPeria support files..."${txtrst}
-cp -a -v -R vendor/XPe/prebuilt/XPeriaSupport/etc/permissions/. out/target/product/"$DEVICE"/system/etc/permissions/
-cp -a -v -R vendor/XPe/prebuilt/XPeriaSupport/framework/. out/target/product/"$DEVICE"/system/framework/
+cp -a -R vendor/XPe/prebuilt/XPeriaSupport/etc/permissions/. out/target/product/"$DEVICE"/system/etc/permissions/
+cp -a -R vendor/XPe/prebuilt/XPeriaSupport/framework/. out/target/product/"$DEVICE"/system/framework/
