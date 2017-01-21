1f9c2cfe3b420df51745e953d2d872539a7ec3fa
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2017-01-20 19:43:54 -0600

    drop grouper, add wt88047

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 add_lunch_combo xpe_a5ul-userdebug
 add_lunch_combo xpe_athene-userdebug
 add_lunch_combo xpe_falcon-userdebug
-add_lunch_combo xpe_grouper-userdebug
 add_lunch_combo xpe_ghost-userdebug
 add_lunch_combo xpe_osprey-userdebug
 add_lunch_combo xpe_titan-userdebug
 add_lunch_combo xpe_addison-userdebug
+add_lunch_combo xpe_wt88047-userdebug
