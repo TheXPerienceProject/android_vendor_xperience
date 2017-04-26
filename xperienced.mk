2333d4e187daa65c0fef3a1e4ad5449c36c68c02
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-04-25 21:59:59 -0500

    disable some functions
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- xperienced.mk
+++ xperienced.mk
@@ -1,3 +1,14 @@
 #Buildtype and changelog
 export XPE_BUILDTYPE := HOMECASE
 export XPERIENCE_CHANGELOG := true
+ifneq ($(XPE_BUILDTYPE), HOMECASE)
+ export WITH_XPERIASUPPORT := true
+else 
+ export WITH_XPERIASUPPORT := false
+endif
+ifneq ($(XPE_BUILDTYPE), UNOFFICIAL)
+ export WITH_XPERIASUPPORT := true
+else 
+ export WITH_XPERIASUPPORT := false
+endif
+
