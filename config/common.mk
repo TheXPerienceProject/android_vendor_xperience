b417e5ba42b5983ecd8ec2535dc10920e089cf63
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-05-02 22:38:20 -0500

    11.1.2_R8
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -412,7 +412,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.xpe.releasetype=$(XPE_BUILDTYPE) \
     ro.modversion=$(XPE_VERSION) \
     ro.xpe.model=$(XPE_BUILD) \
-    ro.xpe.codename=Ak’Kin  \
+    ro.xpe.codename=Niá:wen \
     ro.xpe.cafbranch=lnx.2.1.r14-rel \
     ro.xpelegal.url=http://thexperienceproject.com/legal/
 
