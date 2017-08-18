f4462f015d301b35679c252e29757ec16bb21b8a
Adrian DC <radian.dc@gmail.com>
2017-08-18 19:59:53 +0200

    overlay: Remove default APN overlays
    Change-Id: I7e7e4bc70d37cfd413ab6d9725fc39dc652ed6e9
    Signed-off-by: klozz <klozz707@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the XPerience structure
 xperience_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+        echo '"xperience": {'; \
+        echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
