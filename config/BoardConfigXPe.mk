8c725c0f9ea3b9e2cbf8a4a4e44ecbfbddc157c3
klozz <klozz707@gmail.com>
2017-08-22 21:55:05 -0400

    xpe:move this crap to common
    Change-Id: I048595cbb0c08f07d1e8749b929e26c130946f52
    Signed-off-by: klozz <klozz707@gmail.com>

--- config/BoardConfigXPe.mk
+++ config/BoardConfigXPe.mk
@@ -3,10 +3,3 @@ ifneq ($(WITH_XPE_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.xpe
 endif
 
-ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
-  PRODUCT_PROPERTY_OVERRIDES += \
-    ro.device.cache_dir=/data/cache
-else
-  PRODUCT_PROPERTY_OVERRIDES += \
-    ro.device.cache_dir=/cache
-endif
--- config/common.mk
+++ config/common.mk
@@ -253,6 +253,14 @@ PRODUCT_PACKAGES += \
     libhealthd.xpe
 endif
 
+ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
+  PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.cache_dir=/data/cache
+else
+  PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.cache_dir=/cache
+endif
+
 # ExFAT support
 WITH_EXFAT ?= true
 ifeq ($(WITH_EXFAT),true)
