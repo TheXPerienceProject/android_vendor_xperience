6a3695e9bb1095ca7f647896a745068b7ce7f778
klozz <klozz707@gmail.com>
2017-12-19 02:34:51 -0500

    haxy
    Change-Id: Ie7f8a78c26ad3e994161ff5c5870d60a19f2ab8a
    Signed-off-by: klozz <klozz707@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,7 +1,13 @@
 package android
 type Product_variables struct {
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
 	Needs_text_relocations struct {
 		Cppflags []string
+	}
+	Uses_media_extensions struct {
+		Cflags []string
 	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
@@ -9,6 +15,8 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+    Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Uses_media_extensions   *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
