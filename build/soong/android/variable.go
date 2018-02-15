afbee814b81b786e704839920531889d8299f841
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    lineage: soong: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Change-Id: Ic8722c42807429f2faa3546316c71c40533ce195
    Signed-off-by: klozz <klozz707@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -15,6 +15,9 @@ type Product_variables struct {
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
+	Target_shim_libs struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
@@ -24,4 +27,5 @@ type ProductVariables struct {
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Target_shim_libs  *string `json:",omitempty"`
 }
