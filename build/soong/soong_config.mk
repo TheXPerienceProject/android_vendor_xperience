c05354885ee2b96562d259abf1cf7f6ffcbba1d9
Stricted <info@stricted.net>
2018-02-17 17:28:52 +0100

    lineage: soong: replace space with colon on TARGET_LD_SHIM_LIBS
    that way we can split the shim list logically into multiple lines like
    
    TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
        /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
        /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so
    
    to not have one single huge line
    
    [lineage-17.0]
    this got lost in:
        soong: rework soong_config for Pie
        Change-Id: I3c5001f02975e5487ba66a02217f1d24d2021272
    
    Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -15,12 +15,26 @@ $(call add_json_bool, Needs_legacy_camera_hal1_dyn_native_handle, $(filter true,
 $(call add_json_bool, Needs_text_relocations,                     $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str,  Target_shim_libs,                           $(TARGET_LD_SHIM_LIBS))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+<<<<<<< HEAD
 $(call add_json_str,  Specific_camera_parameter_library,          $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_bool, Target_uses_color_metadata, $(filter true,$(TARGET_USES_COLOR_METADATA)))
 $(call add_json_bool, Uses_generic_camera_parameter_library,      $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
 $(call add_json_bool, Uses_nvidia_enhancements,                   $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy,                       $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device,                     $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+=======
+$(call add_json_str_omitempty, Target_shim_libs, $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_METADATA_AS_FDE_KEY)))
+$(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
+$(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool, Uses_qcom_um_family, $(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_3_18_family, $(filter true,$(TARGET_USES_QCOM_UM_3_18_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_4_family, $(filter true,$(TARGET_USES_QCOM_UM_4_4_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_9_family, $(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_14_family, $(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
+>>>>>>> 47d713d9... lineage: soong: replace space with colon on TARGET_LD_SHIM_LIBS
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
