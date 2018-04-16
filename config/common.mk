2c811d9e563e655f20b1d9b5ede8aa64702b2d6b
klozz <klozz707@gmail.com>
2018-04-16 05:27:07 -0400

    update caf patch and codename
    Change-Id: Icc86dd1817b26093027d9265c581051ca4bcd2fb
    Signed-off-by: klozz <klozz707@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -429,8 +429,8 @@ PRODUCT_GENERIC_PROPERTIES += \
     ro.xpe.channeltype=$(XPERIENCE_CHANNEL) \
     ro.modversion=$(XPE_VERSION) \
     ro.xpe.model=$(XPE_BUILD) \
-    ro.xpe.codename=Calypso \
-    ro.xpe.cafbranch=LA.UM.6.6.r1-06500-89xx.0 \
+    ro.xpe.codename=Elizabeth \
+    ro.xpe.cafbranch=LA.UM.6.6.r1-07400-89xx.0 \
     ro.xpelegal.url=http://thexperienceproject.com/legal/
 
 -include vendor/XPe-priv/keys/keys.mk
