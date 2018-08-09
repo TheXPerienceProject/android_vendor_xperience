2ede69e521d20ff61114ee5ba69eb7e2823d6e4a
klozz <shaclone707@gmail.com>
2018-08-09 19:40:07 -0400

    Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    Signed-off-by: klozz <shaclone707@gmail.com>

--- config/BoardConfigXPerience.mk
+++ config/BoardConfigXPerience.mk
@@ -3,5 +3,10 @@ ifneq ($(WITH_XPE_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.xpe
 endif
 
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
+
 # soong
 include vendor/xperience/build/soong/soong_config.mk
