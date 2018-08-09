49d9741fdc6c4ed3fa2e49248fa622a386fe026e
Simon Shields <simon@lineageos.org>
2018-08-10 01:09:17 +1000

    soong: use a vendor namespace for our variables
    Signed-off-by: klozz <shaclone707@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,16 +1,19 @@
 # Insert new variables inside the XPerience structure
-xperience_soong:
-	$(hide) mkdir -p $(dir $@)
-	$(hide) (\
-	echo '{'; \
-	echo '"Xperience": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
-	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false)'; \
-	echo '},'; \
-	echo '') > $(SOONG_VARIABLES_TMP)
+SOONG_CONFIG_NAMESPACES += Xperience
+
+define add-xperience-var
+$(eval SOONG_CONFIG_Xperience += $(strip $(1)))
+$(eval SOONG_CONFIG_Xperience_$(strip $(1)) := $(2))
+endef
+
+add_xperience_bool = $(eval $(call add-xperience-var,$(1),$(if $(filter true,$(2)),true,false)))
+add_xperience_var = $(eval $(call add-xperience-var,$(1),$(2)))
+
+$(call add_xperience_bool,    Has_legacy_camera_hal1,             $(TARGET_HAS_LEGACY_CAMERA_HAL1))
+$(call add_xperience_bool,    Needs_text_relocations,             $(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS))
+$(call add_xperience_var,     Specific_camera_parameter_library,  $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_xperience_var,     Target_shim_libs,                   $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_xperience_var,     Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
+$(call add_xperience_bool,    Uses_nvidia_enhancements,           $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS))
+$(call add_xperience_bool,    Uses_qcom_bsp_legacy,               $(TARGET_USES_QCOM_BSP_LEGACY))
+$(call add_xperience_bool,    Uses_qti_camera_device,             $(TARGET_USES_QTI_CAMERA_DEVICE))
--- config/BoardConfigXPerience.mk
+++ config/BoardConfigXPerience.mk
@@ -3,3 +3,5 @@ ifneq ($(WITH_XPE_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.xpe
 endif
 
+# soong
+include vendor/xperience/build/soong/soong_config.mk
