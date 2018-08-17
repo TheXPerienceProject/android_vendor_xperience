cb4d617cb4f640b98b277cb5a968207776c0fb45
Simon Shields <simon@lineageos.org>
2018-08-17 07:51:04 -0400

    soong: rework soong_config for Pie
    Change-Id: I89deddaa311b6e79dc9f2406e0ae5da0adc5dd00
    Signed-off-by: klozz <shaclone707@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,19 +1,16 @@
-# Insert new variables inside the XPerience structure
-SOONG_CONFIG_NAMESPACES += Xperience
+_contents := $(_contents)    "Xperience":{$(newline)
 
-define add-xperience-var
-$(eval SOONG_CONFIG_Xperience += $(strip $(1)))
-$(eval SOONG_CONFIG_Xperience_$(strip $(1)) := $(2))
-endef
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
+$(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
+$(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
+$(call add_json_bool, Uses_nvidia_enhancements,              $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
+$(call add_json_bool, Uses_qcom_bsp_legacy,                  $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Uses_qti_camera_device,                $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
-add_xperience_bool = $(eval $(call add-xperience-var,$(1),$(if $(filter true,$(2)),true,false)))
-add_xperience_var = $(eval $(call add-xperience-var,$(1),$(2)))
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
 
-$(call add_xperience_bool,    Has_legacy_camera_hal1,             $(TARGET_HAS_LEGACY_CAMERA_HAL1))
-$(call add_xperience_bool,    Needs_text_relocations,             $(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS))
-$(call add_xperience_var,     Specific_camera_parameter_library,  $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
-$(call add_xperience_var,     Target_shim_libs,                   $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
-$(call add_xperience_var,     Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
-$(call add_xperience_bool,    Uses_nvidia_enhancements,           $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS))
-$(call add_xperience_bool,    Uses_qcom_bsp_legacy,               $(TARGET_USES_QCOM_BSP_LEGACY))
-$(call add_xperience_bool,    Uses_qti_camera_device,             $(TARGET_USES_QTI_CAMERA_DEVICE))
+_contents := $(_contents)    },$(newline)
