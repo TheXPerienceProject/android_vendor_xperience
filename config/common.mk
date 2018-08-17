3e67668ca12c08b9cf3bc3d9b81ae1ad970b5f0a
klozz <shaclone707@gmail.com>
2018-08-17 06:57:01 -0400

    [DNM]remove official verification
    Change-Id: Ia8390ea6be63b923b57962e4f33923467ec61641
    Signed-off-by: klozz <shaclone707@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -164,10 +164,10 @@ PRODUCT_COPY_FILES += \
     vendor/xperience/prebuilt/common/bin/707-xpe.sh:system/addon.d/707-xpe.sh
 
 # Use signing keys for only official builds
-ifeq ($(XPERIENCE_CHANNEL),OFFICIAL)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := .keys/releasekey
-    PRODUCT_OTA_PUBLIC_KEYS = .keys/releasekey/otakey.x509.pem
-endif
+#ifeq ($(XPERIENCE_CHANNEL),OFFICIAL)
+#    PRODUCT_DEFAULT_DEV_CERTIFICATE := .keys/releasekey
+#    PRODUCT_OTA_PUBLIC_KEYS = .keys/releasekey/otakey.x509.pem
+#endif
 
 #XPerience colour :v well not is from xpe but It will be added here so..
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
