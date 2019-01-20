51abc2356eb93b931a05fc46ecbc43ee474d7293
klozz <shaclone707@gmail.com>
2019-01-20 00:44:37 -0500

    fix libqti-util blob
    Signed-off-by: klozz <shaclone707@gmail.com>

--- config/main.mk
+++ config/main.mk
@@ -1,2 +1,10 @@
 # Include XPerience versions
 include vendor/xperience/config/main_version.mk
+
+# MSIM manual provisioning
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
+# TCP Connection Management
+PRODUCT_PACKAGES += tcmiface
+PRODUCT_BOOT_JARS += tcmiface
