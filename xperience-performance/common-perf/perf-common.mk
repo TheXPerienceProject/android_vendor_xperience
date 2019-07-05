31a316ee71814a50fb66ccae6a906f2d01ff80f5
klozz <klozz@vivaldi.net>
2019-07-05 11:44:02 +0000

    perf: only copy 64bit libs if TARGET_ARCH are equals to arm64
    Change-Id: Ib302ead1ea140214201843b1477aeb2deb1a2239
    Signed-off-by: klozz <klozz@vivaldi.net>

--- xperience-performance/common-perf/perf-common.mk
+++ xperience-performance/common-perf/perf-common.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 The XPerience Project
+# Copyright (C) 2018-2019 The XPerience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +26,9 @@ PRODUCT_COPY_FILES += \
     vendor/xperience/xperience-performance/common-perf/lib/libqti-util_system.so:system/lib/libqti-util_system.so \
     vendor/xperience/xperience-performance/common-perf/lib/vendor.qti.hardware.perf@1.0.so:system/lib/vendor.qti.hardware.perf@1.0.so \
     vendor/xperience/xperience-performance/common-perf/lib/vendor.qti.hardware.iop@1.0.so:system/lib/vendor.qti.hardware.iop@1.0.so \
-    vendor/xperience/xperience-performance/common-perf/lib/vendor.qti.hardware.iop@2.0.so:system/lib/vendor.qti.hardware.iop@2.0.so \
+    vendor/xperience/xperience-performance/common-perf/lib/vendor.qti.hardware.iop@2.0.so:system/lib/vendor.qti.hardware.iop@2.0.so
+
+ifeq ($(TARGET_ARCH),arm64)
     vendor/xperience/xperience-performance/common-perf/lib64/libqti-at.so:system/lib64/libqti-at.so \
     vendor/xperience/xperience-performance/common-perf/lib64/libqti-perfd-client_system.so:system/lib64/libqti-perfd-client_system.so \
     vendor/xperience/xperience-performance/common-perf/lib64/libqti-iopd-client_system.so:system/lib64/libqti-iopd-client_system.so\
@@ -35,15 +37,18 @@ PRODUCT_COPY_FILES += \
     vendor/xperience/xperience-performance/common-perf/lib64/vendor.qti.hardware.perf@1.0.so:system/lib64/vendor.qti.hardware.perf@1.0.so \
     vendor/xperience/xperience-performance/common-perf/lib64/vendor.qti.hardware.iop@1.0.so:system/lib64/vendor.qti.hardware.iop@1.0.so \
     vendor/xperience/xperience-performance/common-perf/lib64/vendor.qti.hardware.iop@2.0.so:system/lib64/vendor.qti.hardware.iop@2.0.so
+endif
 
 #vendor
 PRODUCT_COPY_FILES += \
     vendor/xperience/xperience-performance/common-perf/vendor/lib/hw/vendor.qti.hardware.iop@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/vendor.qti.hardware.iop@2.0-impl.so \
     vendor/xperience/xperience-performance/common-perf/vendor/lib/libqti-iopd-client.so:$(TARGET_COPY_OUT_VENDOR)/lib/libqti-iopd-client.so \
     vendor/xperience/xperience-performance/common-perf/vendor/lib/vendor.qti.hardware.iop@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.iop@1.0.so \
-    vendor/xperience/xperience-performance/common-perf/vendor/lib/vendor.qti.hardware.iop@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.iop@2.0.so \
+    vendor/xperience/xperience-performance/common-perf/vendor/lib/vendor.qti.hardware.iop@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.qti.hardware.iop@2.0.so
+
+ifeq ($(TARGET_ARCH),arm64)
     vendor/xperience/xperience-performance/common-perf/vendor/lib64/hw/vendor.qti.hardware.iop@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.hardware.iop@2.0-impl.so \
     vendor/xperience/xperience-performance/common-perf/vendor/lib64/libqti-iopd-client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-iopd-client.so \
     vendor/xperience/xperience-performance/common-perf/vendor/lib64/vendor.qti.hardware.iop@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.iop@1.0.so \
     vendor/xperience/xperience-performance/common-perf/vendor/lib64/vendor.qti.hardware.iop@2.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.iop@2.0.so
-
+endif
