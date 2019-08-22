9ce8a919ef40f821cc5540b7c8914e01896f3f06
klozz <klozz@vivaldi.net>
2019-08-22 05:49:44 +0000

    fix spaces
    Change-Id: I47c36f5665c554923a3eeb638a1b5a354e5d0417

--- config/common.mk
+++ config/common.mk
@@ -2,30 +2,30 @@ RODUCT_BRAND ?= XPerience & MXSeñorPato
 
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))	
 # determine the smaller dimension	
-TARGET_BOOTANIMATION_SIZE := $(shell \	
-  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \	
-    echo $(TARGET_SCREEN_WIDTH); \	
-  else \	
-    echo $(TARGET_SCREEN_HEIGHT); \	
-  fi )	
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
 
  # get a sorted list of the sizes	
-bootanimation_sizes := $(subst .zip,,$(shell ls -1 vendor/xperience/prebuilt/common/bootanimation | sort -rn))	
+bootanimation_sizes := $(subst .zip,,$(shell ls -1 vendor/xperience/prebuilt/common/bootanimation | sort -rn))
 
- # find the appropriate size and set	
-define check_and_set_bootanimation	
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \	
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \	
-    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \	
-      echo $(1); \	
-      exit 0; \	
-    fi;	
-  fi;	
-  echo $(TARGET_BOOTANIMATION_NAME); ))	
-endef	
+ # find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
+    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
 $(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))	
 
-PRODUCT_BOOTANIMATION := vendor/xperience/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip	
+PRODUCT_BOOTANIMATION := vendor/xperience/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
 endif
 
 #well I add ringtones here for all devices
