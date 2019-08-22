3e3db952f1d7672f981d2a97494e6e92300be605
klozz <klozz@vivaldi.net>
2019-08-22 00:17:44 +0000

    update caf tag
    Change-Id: I6567ca79563ce41133285e5ea4b4b41d718f75ac
    Signed-off-by: klozz <klozz@vivaldi.net>

--- config/main_version.mk
+++ config/main_version.mk
@@ -7,12 +7,12 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.modversion=$(XPE_VERSION) \
     ro.xpe.model=$(XPE_BUILD) \
     ro.xpe.codename=Cernunnos \
-    ro.xpe.cafbranch=LA.UM.7.1.r1-15800-sm8150.0 \
+    ro.xpe.cafbranch=LA.UM.7.1.r1-16300-sm8150.0 \
     ro.xpelegal.url=http://thexperienceproject.com/legal/
 
 XPE_DISPLAY_VERSION := $(XPE_VERSION)
 
-CAF_BRANCH := LA.UM.7.1.r1-15800-sm8150.0
+CAF_BRANCH := LA.UM.7.1.r1-16300-sm8150.0
 
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.xpe.display.version=$(XPE_DISPLAY_VERSION)
