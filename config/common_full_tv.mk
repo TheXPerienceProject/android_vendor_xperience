fa0b3022987a76ed70c4b453e06ea570137910f1
Aaron Kling <webgeek1234@gmail.com>
2019-08-30 20:44:20 -0500

    Disable AudioFX on atv
    The interface is not usable on atv
    
    Change-Id: Ibe2c33e5528b593ae338a62cee62e6fe9cfe47ad

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,9 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/xperience/config/common_full.mk)
 
+# Exclude AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/xperience/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -5,3 +5,6 @@ ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/xperience/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
 endif
+
+# Exclude AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
