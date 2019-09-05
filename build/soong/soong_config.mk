d6f81a7182589e80a0220f8af0ce47e3f776d1c0
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    xperience: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c
    Signed-off-by: klozz <klozz@vivaldi.net>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,8 @@
-_contents := $(_contents)    "Xperience":{$(newline)
 
 # Target variables default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
+
+_json_contents := $(_json_contents)    "Xperience":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str, Additional_gralloc_10_usage_bits,            $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
@@ -22,6 +23,6 @@ $(call add_json_bool, Uses_qcom_bsp_legacy,                       $(filter true,
 $(call add_json_bool, Uses_qti_camera_device,                     $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
