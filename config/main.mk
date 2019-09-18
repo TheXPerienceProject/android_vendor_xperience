b8b13696db4a0cd01187c4e3726c05fecf7c443a
klozz <klozz@vivaldi.net>
2019-09-18 02:31:47 +0000

    xperience: drop product packages and boot jars
    Change-Id: Ib63fa81b6ca558a98bcf5d3c79da73a2a58319f0
    Signed-off-by: klozz <klozz@vivaldi.net>

--- config/main.mk
+++ config/main.mk
@@ -2,9 +2,9 @@
 include vendor/xperience/config/main_version.mk
 
 # MSIM manual provisioning
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
+#PRODUCT_PACKAGES += telephony-ext
+#PRODUCT_BOOT_JARS += telephony-ext
 
 # TCP Connection Management
-PRODUCT_PACKAGES += tcmiface
-PRODUCT_BOOT_JARS += tcmiface
+#PRODUCT_PACKAGES += tcmiface
+#PRODUCT_BOOT_JARS += tcmiface
