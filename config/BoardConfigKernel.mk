Flamingo-OS/vendor_flamingo

kernel: use dtc from prebuilt
dtc from OUT_DIR might not be available, so use the prebuilt one instead.

Also, clean-up a little bit and follow the same logic as prebuilt make binary.

Change-Id: I48dd57d7e69a2dbf97c7bd3b225eaf4ec8b41e23
Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

config/BoardConfigKernel.mk
@@ -141,6 +141,9 @@ else ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
 BOARD_PREBUILT_DTBOIMAGE ?= $(PRODUCT_OUT)/dtbo-pre.img
 endif
 
+# Set use the full path to the dtc command
+KERNEL_DTC_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/dtc
+
 # Set use the full path to the make command
 KERNEL_MAKE_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/make
 

TheXPerienceProject/android_vendor_xperience

Signed-off-by: klozz <klozz@vivaldi.net>

config/BoardConfigKernel.mk
@@ -150,6 +150,9 @@ else ifeq ($(BOARD_KERNEL_SEPARATED_DTBO),true)
 BOARD_PREBUILT_DTBOIMAGE ?= $(PRODUCT_OUT)/dtbo-pre.img
 endif
 
+# Set use the full path to the dtc command
+KERNEL_DTC_CMD := $(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_OS)-x86/bin/dtc
+
 # Set use the full path to the make command
 KERNEL_MAKE_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_OS)-x86/bin/make
 