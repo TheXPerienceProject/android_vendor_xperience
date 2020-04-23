c7405c822a368a5158079307b3c293c8cd5ba01a
klozz <klozz@vivaldi.net>
2020-04-23 13:11:21 +0000

    xperience: make hidl base and manager as common some people build xpe and forget to enable , still some blobs need them
    Signed-off-by: klozz <klozz@vivaldi.net>
    Change-Id: Ib588ca07d9648f43a657841f489c2a965205faa6

--- config/common.mk
+++ config/common.mk
@@ -535,3 +535,9 @@ ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
   ALLOW_MISSING_DEPENDENCIES := true
 endif
 
+# HIDL 
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
