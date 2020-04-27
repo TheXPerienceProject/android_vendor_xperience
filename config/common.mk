b9af905b4b185eb105f059d4a5b79be7295efc5a
klozz <klozz@vivaldi.net>
2020-04-27 10:49:35 +0000

    remove faceunlock
    Signed-off-by: klozz <klozz@vivaldi.net>
    Change-Id: I17d941fde212ae6d3ee1c8ebfdeb2fc40af17947

--- config/common.mk
+++ config/common.mk
@@ -188,20 +188,6 @@ PRODUCT_PACKAGES += \
 
 endif
 
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= false
-
-ifeq ($(TARGET_XPE_ARCH), arm64)
-ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
-PRODUCT_PACKAGES += \
-    FaceUnlockService
-TARGET_FACE_UNLOCK_SUPPORTED := true
-endif
-endif
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
-
 # TWRP
 ifeq ($(WITH_TWRP),true)
 include vendor/xperience/config/twrp.mk
