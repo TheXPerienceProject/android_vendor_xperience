9e73717c4643db9991536f55a8af68883f427ebb
Jake Weinstein <jake@aospa.co>
2020-06-27 22:04:30 +0200

    packages:build libprotobuf-cpp-full-rtti
    Supposedly this is used for the Neural Network HAL
    
    Change-Id: I2bf45b0441ed3824c2757212fa9d2926b5614a5e
    
    Change-Id: I85902915995dda1ebb9e9cf9d8eb685ca8670dec
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/packages.mk
+++ config/packages.mk
@@ -256,3 +256,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xperience_themes/product/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/xperience_themes/product/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Neural Network
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-rtti
