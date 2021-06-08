6a5413be2dbe6a6dc7039efc31036836eac707d3
klozz <klozz@TheXPerienceProject.org>
2021-06-09 00:09:47 +0200

    switch to codename dragur
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,9 +6,13 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.xpe.channeltype=$(XPERIENCE_CHANNEL) \
     ro.modversion=$(XPE_VERSION) \
     ro.xpe.model=$(XPERIENCE_BUILD) \
-    ro.xpe.codename=Red-Velvet-Cake \
+    ro.xpe.codename=Draugr \
     ro.xpelegal.url=http://thexperienceproject.org/legal/ \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
+ADDITIONAL_BUILD_PROPERTIES += \
+   ro.qcom.system=$(shell grep "refs/tags/LA.QSSI" .repo/manifests/snippets/caf.xml | cut -d '"' -f2 | cut -d "/" -f3) \
+   ro.qcom.vendor=$(shell grep "refs/tags/LA.UM" .repo/manifests/snippets/caf.xml | cut -d '"' -f2 | cut -d "/" -f3) 
 
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
