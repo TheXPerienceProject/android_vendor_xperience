2af78424b44fa70e266bc4eb73ab367f3428b028
Michael Bestas <mkbestas@lineageos.org>
2021-10-05 23:37:55 +0300

    build: Replace obsolete ADDITIONAL_BUILD_PROPERTIES
    Fixes:
    error: ADDITIONAL_BUILD_PROPERTIES is obsolete.  Please use ADDITIONAL_SYSTEM_PROPERTIES.
    Change-Id: I7eb3c47d9fd705aa8f8d39e10563e47ffbf2d560build:
    
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,5 +1,5 @@
 # Apply it to build.prop
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.xpe.version=$(XPE_VERSION) \
     ro.xpe.releasetype=$(XPE_BUILDTYPE) \
     ro.xperience.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
@@ -10,19 +10,19 @@ ADDITIONAL_BUILD_PROPERTIES += \
     ro.xpelegal.url=http://thexperienceproject.org/legal/ \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
    ro.qcom.system=$(shell grep "refs/tags/LA.QSSI" .repo/manifests/snippets/caf.xml | cut -d '"' -f2 | cut -d "/" -f3) \
    ro.qcom.vendor=$(shell grep "refs/tags/LA.UM" .repo/manifests/snippets/caf.xml | cut -d '"' -f2 | cut -d "/" -f3) 
 
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 XPE_DISPLAY_VERSION := $(XPE_VERSION)
 
 # Gaussian Blur
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
