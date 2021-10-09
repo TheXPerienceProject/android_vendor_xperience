d50585b7fb845541ef88311ce2b981e900c0db83
klozz <klozz@thexperienceproject.org>
2021-10-09 15:08:53 +0200

    Fixes to pass the build Change-Id: I2ec439990b74cf099a78832fef4b7bc8dd17993c
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- config/common.mk
+++ config/common.mk
@@ -143,3 +143,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 $(foreach f,$(wildcard vendor/xperience/prebuilt/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
+# Optimize everything for preopt
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+
--- config/packages.mk
+++ config/packages.mk
@@ -23,8 +23,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # CellBroadcastReceiver
 PRODUCT_PACKAGES += \
