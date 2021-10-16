60c1b7c7559e3150fe1e914e9b29b0e325940d61
zljing <zljing@codeaurora.org>
2021-10-16 05:26:45 +0200

    common:properties: Enable dex2oat64 to do dexopt
    CR-Fixed: 2989784
    Change-Id: Ibe30b53251b26d611ce93714d9521a933aca2227
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- config/common.mk
+++ config/common.mk
@@ -80,6 +80,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 
+# Enable dex2oat64 to do dexopt
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
+
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off
