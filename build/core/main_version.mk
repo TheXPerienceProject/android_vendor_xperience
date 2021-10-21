20866a76dd128486bea245064ecaea5365a82bfd
klozz <klozz@thexperienceproject.org>
2021-10-21 06:52:55 +0200

    Wheather: remove tag  <uses-library> test.runner and device
    due to the build needs uses_libs and we aren't building those jar files then i removed them from the manifest
    
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -29,3 +29,8 @@ XPE_DISPLAY_VERSION := $(XPE_VERSION)
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
+
+ifeq ($(XPERIENCE_CHANNEL),OFFICIAL)
+# Signing
+-include vendor/xperience/signing/keys.mk
+endif
