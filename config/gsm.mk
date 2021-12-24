083e5dc6248e30e6f5234085b9218129122cd7d8
klozz <xXx.Reptar.Rawrr.xXx@gmail.com>
2021-12-23 23:04:29 -0600

    move ims symlink to separated file
    Change-Id: I1fb3ae399f7cf479318d61335704b08c0e994d55
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- config/gsm.mk
+++ config/gsm.mk
@@ -53,5 +53,3 @@ PRODUCT_PACKAGES += \
 #    tcmiface \
 #    telephony-ext
 
-$(shell mkdir -p $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/ && pushd $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64 > /dev/null && ln -fs /system_ext/lib64/libimscamera_jni.so libimscamera_jni.so && popd > /dev/null)
-$(shell mkdir -p $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/ && pushd $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64 > /dev/null && ln -fs /system_ext/lib64/libimsmedia_jni.so libimsmedia_jni.so && popd > /dev/null)
