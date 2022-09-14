c0f30f2eefe451623add8c2e80ea68946e643f65
Aaron Kling <webgeek1234@gmail.com>
2022-09-13 20:07:20 -0500

    config: Exclude product_manifest.xml from enforced existing packages
    Not all build targets have this target modules
    
    Change-Id: I01051bb0f6e285ca64bfbfbbaa70bfab796533eb
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/common.mk
+++ config/common.mk
@@ -165,7 +165,7 @@ SQUISHER_SCRIPT := vendor/xperience/tools/squisher
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(XPERIENCE_BUILD)/$(TARGET_PRODUCT).mk),)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(XPERIENCE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml)
 
 # Copy all xperience-specific init rc files
 $(foreach f,$(wildcard vendor/xperience/prebuilt/etc/init/*.rc),\
