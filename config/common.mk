4d6e8c8365bcc8f5932db0ca825c830915c34dfe
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-03 21:55:15 -0400

    common: Allow opting out of EPPE
    * All Pixel devices fail to pass this check, allow disabling this
      instead of making tons of modifications on the device trees.
    
    Change-Id: I9a6fcc0a05b6ea90818a42d5c9500663db864542
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/common.mk
+++ config/common.mk
@@ -164,8 +164,10 @@ SQUISHER_SCRIPT := vendor/xperience/tools/squisher
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(XPERIENCE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
+endif
 
 # Copy all xperience-specific init rc files
 $(foreach f,$(wildcard vendor/xperience/prebuilt/etc/init/*.rc),\
