bc4f7af7410707a0ec1aa9b956a0588b116fad4c
Alessandro Astone <ales.astone@gmail.com>
2022-12-31 22:08:16 +0100

    kernel: Force disable LLVM CUDA
    The kernel tries to define the LINUX_COMPILER macro from the output
    of `$(CC) -v | grep ' version '`, but in clang that might produce
    more than one line containing ' version ' when CUDA is installed:
    
    ```
    Android (8490178, based on r450784d) clang version 14.0.6
    Found CUDA installation: /usr/local/cuda, version 11.0
    ```
    
    That causes the macro to expand into multiple lines, upsetting the
    compiler that cannot find the ending double quote.
    
    In upstream linux this is fixed from v5.8
    
    Change-Id: Icde4253e989df1cbbefc0ce98f437d16822fc93b
    
    Change-Id: I9221fa5ea3fc9991237ec474a2ec75e82e529f5f
    Signed-off-by: klozz <carlosj@klozz.dev>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -260,7 +260,7 @@ ifneq ($(TARGET_KERNEL_CLANG_COMPILE),false)
         PATH_OVERRIDE += PATH=$(TARGET_KERNEL_CLANG_PATH)/bin:$$PATH
     endif
     ifeq ($(KERNEL_CC),)
-        KERNEL_CC := CC="$(CCACHE_BIN) clang"
+        KERNEL_CC := CC="$(CCACHE_BIN) clang --cuda-path=/dev/null"
     endif
     ifeq ($(KERNEL_LD),)
         KERNEL_LD :=
