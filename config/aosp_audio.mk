add9a19f627530487136dfd170171d217ba52aec
klozz <carlosj@klozz.dev>
2023-02-17 22:37:26 -0600

    add extra copy to aosp audio
    Change-Id: If2a96f544098a42e79ef21dcea94c51d345dae47
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/aosp_audio.mk
+++ config/aosp_audio.mk
@@ -1,5 +1,6 @@
 # Copyright 2013 The Android Open Source Project
 # Copyright 2019 The LineageOS Project
+# Copyright 2023 The XPerience Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
