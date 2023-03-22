a4cd0a4baad3df7df213b9980679bb942f3cfe60
klozz <carlosj@klozz.dev>
2023-03-22 03:09:57 -0600

    update codename
    Change-Id: I9b294e85de06db5c4eec4d337919bb1f6b7a3003
    Signed-off-by: klozz <carlosj@klozz.dev>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,8 +6,8 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.xpe.channeltype=$(XPERIENCE_CHANNEL) \
     ro.modversion=$(XPE_VERSION) \
     ro.xpe.model=$(XPERIENCE_BUILD) \
-    ro.xpe.codename=Taerve-Uni \
-    ro.xpelegal.url=http://thexperienceproject.org/legal/ \
+    ro.xpe.codename=NCC-1701​ \
+    ro.xpelegal.url=http://thexperienceproject.klozz.dev/legal/ \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
 ADDITIONAL_SYSTEM_PROPERTIES += \
