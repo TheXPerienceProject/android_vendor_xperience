ba8665ab518d1ce31237d07d812b053fb3e3f31b
klozz <carlosj@klozz.dev>
2023-10-14 04:49:29 -0600

    Track 5.15 board and remove pre-um family
    Change-Id: I460a35fa32424e7ff1dfc33f4eab7d7028268efe
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -9,17 +9,18 @@ ATOLL := atoll #SM6250
 LAHAINA := lahaina #SM8350
 HOLI := holi #SM4350
 TARO := taro #SM8450
+PARROT := parrot #SM6450
+KALAMA := kalama #SM8550
 
-B_FAMILY := msm8226 msm8610 msm8974
-B64_FAMILY := msm8992 msm8994
-BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
 UM_4_19_FAMILY := $(KONA) $(LITO) $(BENGAL)
 UM_5_4_FAMILY := $(LAHAINA) $(HOLI)
-UM_5_10_FAMILY := $(TARO)
+UM_5_10_FAMILY := $(TARO) $(PARROT)
+UM_5_15_FAMILY := $(KALAMA)
+
 UM_PLATFORMS := \
     $(UM_3_18_FAMILY) \
     $(UM_4_4_FAMILY) \
@@ -27,13 +28,17 @@ UM_PLATFORMS := \
     $(UM_4_14_FAMILY) \
     $(UM_4_19_FAMILY) \
     $(UM_5_4_FAMILY) \
-    $(UM_5_10_FAMILY)
+    $(UM_5_10_FAMILY) \
+    $(UM_5_15_FAMILY)
+
 QSSI_SUPPORTED_PLATFORMS := \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
     $(UM_4_19_FAMILY) \
     $(UM_5_4_FAMILY) \
-    $(UM_5_10_FAMILY)
+    $(UM_5_10_FAMILY) \
+    $(UM_5_15_FAMILY)
+
 PORTED_TO_UM_4_19_FAMILY := msm8953 msm8937 sdm660
 
 ifeq (,$(TARGET_ENFORCES_QSSI))
@@ -77,21 +82,8 @@ SOONG_CONFIG_qtidisplay_var1 ?= false
 SOONG_CONFIG_qtidisplay_var2 ?= false
 SOONG_CONFIG_qtidisplay_var3 ?= false
 
-# UM platforms no longer need this set on O+
-ifneq ($(filter $(B_FAMILY) $(B64_FAMILY) $(BR_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    TARGET_USES_QCOM_BSP := true
-endif
-
 # Tell HALs that we're compiling an AOSP build with an in-line kernel
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-
-ifneq ($(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
-    TARGET_USES_QCOM_BSP_LEGACY := true
-    # Enable legacy audio functions
-    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-        USE_CUSTOM_AUDIO_POLICY := 1
-    endif
-endif
 
 # Enable media extensions
 TARGET_USES_MEDIA_EXTENSIONS := true
@@ -99,10 +91,8 @@ TARGET_USES_MEDIA_EXTENSIONS := true
 # Allow building audio encoders
 TARGET_USES_QCOM_MM_AUDIO := true
 
-# Enable color metadata for every UM platform
-ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+# Enable color metadata
     TARGET_USES_COLOR_METADATA := true
-endif
 
 # Enable DRM PP driver on UM platforms that support it
 ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
@@ -120,11 +110,6 @@ ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
 endif
 
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
-
-# Mark GRALLOC_USAGE_HW_2D as valid gralloc bit on legacy platforms that support it
-ifneq ($(filter msm8960 msm8952 $(B_FAMILY) $(B64_FAMILY) $(BR_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 10)
-endif
 
 # Enable displayconfig on every UM platform
 ifeq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -145,16 +130,7 @@ endif
 # List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)
 
-ifneq ($(filter $(B_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    MSM_VIDC_TARGET_LIST := $(B_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8974
-else ifneq ($(filter $(B64_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    MSM_VIDC_TARGET_LIST := $(B64_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8994
-else ifneq ($(filter $(BR_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    MSM_VIDC_TARGET_LIST := $(BR_FAMILY)
-    QCOM_HARDWARE_VARIANT := msm8916
-else ifneq ($(filter $(UM_3_18_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter $(UM_3_18_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_3_18_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8996
 else ifneq ($(filter $(UM_4_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
@@ -173,6 +149,8 @@ else ifneq ($(filter $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := sm8350
 else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := sm8450
+else ifneq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+    QCOM_HARDWARE_VARIANT := sm8550
 else
     MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
