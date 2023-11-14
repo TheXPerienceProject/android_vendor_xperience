f69908ac36190e69c85868c88d7266512cb08b1f
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:34:06 -0500

    signing: support inline signing for U [2/2]
    For those who have used .android-certs previously, cp .android-certs/* vendor/xperience/signing/keys/
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/version.mk
+++ config/version.mk
@@ -96,12 +96,12 @@ endif
 ###########################################################################
 # Signing
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+fneq (,$(wildcard vendor/xperience/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/xperience/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/xperience/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/xperience/signing/keys/otakey.x509.pem
 endif
 endif
 #########################################################################
