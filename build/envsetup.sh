7b64f303f73d7eadc4b227c74a0bc3e52df46aa2
klozz <carlosj@klozz.dev>
2023-12-17 07:21:08 +0000

    add aospremote
    Signed-off-by: klozz <carlosj@klozz.dev>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -275,21 +275,30 @@ function lineageremote() {
 }
 
 function aospremote() {
-  if ! git rev-parse --git-dir &>/dev/null; then
-    echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
-    return 1
-  fi
-  git remote rm aosp 2>/dev/null
-  local PROJECT=$(pwd -P | sed -e "s#$ANDROID_BUILD_TOP\/##; s#-caf.*##; s#\/default##")
-  # Google moved the repo location in Oreo
-  if [ $PROJECT = "build/make" ]; then
-    PROJECT="build"
-  fi
-  if (echo $PROJECT | grep -qv "^device"); then
-    local PFX="platform/"
-  fi
-  git remote add aosp https://android.googlesource.com/$PFX$PROJECT
-  echo "Remote 'aosp' created"
+    if ! git rev-parse --git-dir &> /dev/null
+    then
+        echo ".git directory not found. Please run this from the root directory of the Android repository you wish to set up."
+        return 1
+    fi
+    git remote rm aosp 2> /dev/null
+
+    if [ -f ".gitupstream" ]; then
+        local REMOTE=$(cat .gitupstream | cut -d ' ' -f 1)
+        git remote add aosp ${REMOTE}
+    else
+        local PROJECT=$(pwd -P | sed -e "s#$ANDROID_BUILD_TOP\/##; s#-caf.*##; s#\/default##")
+        # Google moved the repo location in Oreo
+        if [ $PROJECT = "build/make" ]
+        then
+            PROJECT="build"
+        fi
+        if (echo $PROJECT | grep -qv "^device")
+        then
+            local PFX="platform/"
+        fi
+        git remote add aosp https://android.googlesource.com/$PFX$PROJECT
+    fi
+    echo "Remote 'aosp' created"
 }
 
 function cafremote() {
