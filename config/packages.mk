f295eb7b47490d17257e575417a3b6e95ca949af
klozz <carlosj@klozz.dev>
2024-07-15 20:04:56 -0600

    config: add free form window
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/packages.mk
+++ config/packages.mk
@@ -546,3 +546,7 @@ CUSTOM_LOCALES += \
 
 #Microsoft Windows link
 $(call inherit-product, vendor/xperience/prebuilt/microsoft/packages.mk)
+
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
