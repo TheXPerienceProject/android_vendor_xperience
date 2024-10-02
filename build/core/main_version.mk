4d2ee1fe2508e0306ef17ee20f86355cf2b771cc
klozz <carlosj@klozz.dev>
2024-10-02 09:58:39 +0000

    main_version: move  properties to product
    ADDITIONAL_SYSTEM_PROPERTIES is being deprecated.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,5 +1,5 @@
 # Apply it to build.prop
-ADDITIONAL_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.xpe.version=$(XPE_VERSION) \
     ro.xpe.releasetype=$(XPE_BUILDTYPE) \
     ro.xperience.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
@@ -13,13 +13,13 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
 # Settings props
-#ADDITIONAL_SYSTEM_PROPERTIES += \
+#PRODUCT_PRODUCT_PROPERTIES += \
     ro.xpe.battery?=$(XPERIENCE_BATTERY) \
     ro.xpe.chipset?=$(XPERIENCE_CHIPSET) \
     ro.xpe.display_resolution?=$(XPERIENCE_DISPLAY) \
     ro.xpe.maintainer?=$(XPERIENCE_MAINTAINER)
 
-ADDITIONAL_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.qcom.system=$(shell grep "refs/tags/LA.QSSI" .repo/manifests/default.xml | cut -d '"' -f2 | cut -d "/" -f3) \
     ro.qcom.vendor=$(shell grep "refs/tags/LA.VENDOR" .repo/manifests/default.xml | cut -d '"' -f2 | cut -d "/" -f3) 
 
@@ -28,17 +28,17 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
 
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-ADDITIONAL_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
 
 XPE_DISPLAY_VERSION := $(XPE_VERSION)
 
 # Gaussian Blur
-ADDITIONAL_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 
