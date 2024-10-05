728205a528b4d76d840ac4d8a77be810f3d4470e
klozz <carlosj@klozz.dev>
2024-10-05 05:48:18 +0000

    common: remove Key "BUILD_UTC_DATE" isn't a valid prop override
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/common.mk
+++ config/common.mk
@@ -39,7 +39,7 @@ endif
 
 # Fixes: terminate called after throwing an instance of 'std::out_of_range' what(): basic_string::erase
 # error with prop override
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+#PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # general properties
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
