e5f0e921009188573c186e1ece21840e00d2af9f
klozz <carlosj@klozz.dev>
2025-03-10 18:22:53 -0600

    xperience: Enable LHDC by per QCOM family
    supported 5.4 to 6.1 family
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/BoardConfigXPerience.mk
+++ config/BoardConfigXPerience.mk
@@ -12,3 +12,24 @@ DISABLE_EAP_PROXY := true
 # Default mount point symlinks to false
 # since they are not used on 8998 and up
 TARGET_MOUNT_POINTS_SYMLINKS ?= false
+
+# auto define LHDC
+ifneq ($(filter $(UM_6_1_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+$(warning Enable LHDC on $(UM_6_1_FAMILY))
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/savitech/lhdc/6.1
+else ifneq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+$(warning Enable LHDC on $(UM_5_15_FAMILY))
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/savitech/lhdc/5.15
+else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+$(warning Enable LHDC on $(UM_5_10_FAMILY))
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/savitech/lhdc/5.10
+else ifneq ($(filter $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+$(warning Enable LHDC on $(UM_5_4_FAMILY))
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/savitech/lhdc/5.4
+else
+ $(warning No LHDC support detected you can enable it manually just set the correct namespace)
+endif
