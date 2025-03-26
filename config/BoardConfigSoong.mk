db8d936249407a7477281117b7e7d8b754a6d4ce
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 20:06:00 +0200

    soong: Move QTI gralloc configs to select()
    Change-Id: Icb28d5c2aa5d51860799b87b7b9b04c48014f7f4
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,9 +31,6 @@ SOONG_CONFIG_xperienceGlobalVars += \
     bootloader_message_offset \
     additional_gralloc_10_usage_bits \
     camera_override_format_from_reserved \
-    gralloc_handle_has_custom_content_md_reserved_size \
-    gralloc_handle_has_reserved_size \
-    gralloc_handle_has_ubwcp_format \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
@@ -59,9 +56,6 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_xperienceGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
-SOONG_CONFIG_xperienceGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
-SOONG_CONFIG_xperienceGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
-SOONG_CONFIG_xperienceGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_xperienceGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 SOONG_CONFIG_xperienceGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 
@@ -69,9 +63,6 @@ SOONG_CONFIG_xperienceGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
-TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE ?= false
-TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
-TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT ?= false
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED ?= 1
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED ?= 0
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS ?= true
