f91f86660cb731dfd39fe5bf86ece5f0c99eecc6
techyminati <sinha.aryan03@gmail.com>
2025-06-14 23:30:42 +0800

    config: Enable Material Design 3 Expressive
    Change-Id: Ib6d3ac2e0f3d76768c508e1b00efc51e141c1b09
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/common.mk
+++ config/common.mk
@@ -73,6 +73,10 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.egl.blobcache.multifile=true \
     ro.egl.blobcache.multifile_limit=33554432
 
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
+
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
