08f8e8af5f9fbcc3224291995093f89229b5e9c7
klozz <carlosj@klozz.dev>
2025-07-01 04:44:02 -0600

    packages: return quickacceswallet
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/packages.mk
+++ config/packages.mk
@@ -68,8 +68,8 @@ PRODUCT_PACKAGES += \
 
 # Wallet app for Power menu integration
 # https://source.android.com/devices/tech/connect/quick-access-wallet
-#PRODUCT_PACKAGES += \
-#    QuickAccessWallet
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
 
 # Use signing keys for only official builds
 ifeq ($(XPERIENCE_CHANNEL),OFFICIAL)
