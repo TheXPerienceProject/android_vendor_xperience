VoltageOS-staging/vendor_voltage

vendor: boost framework configs
Signed-off-by: Dmitrii <bankersenator@gmail.com>

config/common.mk
@@ -266,6 +266,9 @@ include vendor/voltage/config/version.mk
 # BootAnimation
 include vendor/voltage/config/bootanimation.mk
 
+# boost framework
+include vendor/voltage/config/boost.mk
+
 # Fonts
 $(call inherit-product, vendor/voltage/fonts/fonts.mk)
 

TheXPerienceProject/android_vendor_xperience

Signed-off-by: klozz <carlosj@klozz.dev>

config/common.mk
@@ -2,6 +2,9 @@ PRODUCT_BRAND ?= XPerience
 # Versioning
 -include vendor/xperience/config/version.mk
 
+# boost framework
+include vendor/xperience/config/boost.mk
+
 # Include Common Qualcomm Device Tree.
 $(call inherit-product, device/xperience/common/common.mk)
 