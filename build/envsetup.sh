6d071322d3e9e80b9d5404362255d8e7d091a47d
Nick Reuter <nreuter85@gmail.com>
2025-08-24 21:51:32 -0500

    build: Update rust toolchain dynamically
    Change-Id: Iedcc0ebc7f42f65bd1b9578d92bf101f642dec7e
    Signed-off-by: klozz <carlosj@klozz.dev>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -2,6 +2,9 @@
 CLANG_VERSION=$(build/soong/scripts/get_clang_version.py)
 export LLVM_AOSP_PREBUILTS_VERSION="${CLANG_VERSION}"
 
+RUST_VERSION=$(grep 'RustDefaultVersion =' build/soong/rust/config/global.go | awk '{print $3}' | awk -F '"' '{print $2}')
+export RUST_AOSP_PREBUILTS_VERSION="${RUST_VERSION}"
+
 function __print_xperience_functions_help() {
   cat <<EOF
 Additional LineageOS functions:
