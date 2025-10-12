LineageOS/android_vendor_lineage

audio: Add Plasma Mobile alarms & notifications
In the interest of customization, here are a bunch more alarms and notifications sounds. The code is taken from ProtonAOSP, and adapted to Lineage.
Cherry-picked from: https://github.com/ProtonAOSP/android_vendor_proton/commit/629b35007c1b1dd640f32b0b8ec85be3b1c5657d
Cherry-picked from: https://github.com/ProtonAOSP/android_vendor_proton/commit/6f203a966ffece1c05dd86199e00b51b35329954

Change-Id: Iaacb0d2e94d2dda1cf90cabb9368cea1a4cf0a7f
Authored-by: Danny Lin <danny@kdrag0n.dev>
Co-Authored-by: Inhishonor <inhishonor@protonmail.com>

config/common.mk
@@ -242,6 +242,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.enable_transaction_tracing=false
 endif
 
+# Audio files
+$(call inherit-product, vendor/lineage/audio/audio.mk)
+
 # SetupWizard
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.theme=glif_v4 \

TheXPerienceProject/android_vendor_xperience

Signed-off-by: klozz <carlosj@klozz.dev>

config/common.mk
@@ -207,6 +207,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     debug.sf.frame_rate_multiple_threshold=60 \
     ro.surface_flinger.enable_frame_rate_override=false
 
+# Audio files
+$(call inherit-product, vendor/xperience/audio/audio.mk)
 
 # Rescue Party
 # Disable RescueParty due to high risk of data loss