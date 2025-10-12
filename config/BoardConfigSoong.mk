LineageOS/android_vendor_lineage

charger: Convert to Android.bp
Change-Id: I6bc9e33545942cc9c7a320e44d625a724ed4ae53

config/BoardConfigSoong.mk
@@ -25,6 +25,20 @@ ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
 endif
 
+# Charger
+lineage_charger_density := mdpi
+ifneq (,$(TARGET_SCREEN_DENSITY))
+lineage_charger_density := $(strip \
+  $(or $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 560))),1),xxxhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 400))),1),xxhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 280))),1),xhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 200))),1),hdpi,mdpi)))
+else ifneq (,$(filter mdpi hdpi xhdpi xxhdpi xxxhdpi,$(PRODUCT_AAPT_PREF_CONFIG)))
+# If PRODUCT_AAPT_PREF_CONFIG includes a dpi bucket, then use that value.
+lineage_charger_density := $(PRODUCT_AAPT_PREF_CONFIG)
+endif
+$(call soong_config_set,lineage_charger,density,$(lineage_charger_density))
+
 # Libui
 ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
     $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))

TheXPerienceProject/android_vendor_xperience

Signed-off-by: klozz <carlosj@klozz.dev>

config/BoardConfigSoong.mk
@@ -73,6 +73,20 @@ ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
     $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
 endif
 
+# Charger
+xperience_charger_density := mdpi
+ifneq (,$(TARGET_SCREEN_DENSITY))
+xperience_charger_density := $(strip \
+  $(or $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 560))),1),xxxhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 400))),1),xxhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 280))),1),xhdpi),\
+       $(if $(filter $(shell echo $$(($(TARGET_SCREEN_DENSITY) >= 200))),1),hdpi,mdpi)))
+else ifneq (,$(filter mdpi hdpi xhdpi xxhdpi xxxhdpi,$(PRODUCT_AAPT_PREF_CONFIG)))
+# If PRODUCT_AAPT_PREF_CONFIG includes a dpi bucket, then use that value.
+xperience_charger_density := $(PRODUCT_AAPT_PREF_CONFIG)
+endif
+$(call soong_config_set,xperience_charger,density,$(xperience_charger_density))
+
 # Libui
 ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
     $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))