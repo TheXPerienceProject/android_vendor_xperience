ed8de54811b1fe1a64eca6f562c528359feefbab
klozz <carlosj@klozz.dev>
2025-10-24 00:43:32 -0600

    config:overlay: Add pif overlay
    Change-Id: I502867819c28c56ddb774863ad0860418a42da38
    Signed-off-by: klozz <carlosj@klozz.dev>

--- config/common.mk
+++ config/common.mk
@@ -267,18 +267,5 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # sony extra features
 $(call inherit-product-if-exist, vendor/sony/extra/extra.mk)
 
-# PIF values
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.pihooks_MANUFACTURER?=Google \
-    persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=caiman_beta \
-    persist.sys.pihooks_DEVICE?=caiman \
-    persist.sys.pihooks_ID?=BP31.250610.004 \
-    persist.sys.pihooks_RELEASE?=12 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-07-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
-    persist.sys.pihooks_SDK_INT?=32
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/caiman_beta/caiman:16/BP31.250610.004/13769805:user/release-keys" \
-    PihooksGmsModel="Pixel 9 Pro"
+# PIF
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/xperience/overlay/certification
